80fde3d25e0f90651bfae97233042e1eedae6644
3Peet <mypeet096@gmail.com>
2025-11-11 15:18:41 +0700

    style: format code without semicolon

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View } from "react-native"
 
 export default function Index() {
 	return (
@@ -7,5 +7,5 @@ export default function Index() {
 				Welcome to Nativewind!
 			</Text>
 		</View>
-	);
+	)
 }
